73a78b577f2f1d246799b3b56fbb1485f51767d1
Yoannou <yoannou300@gmail.com>
2021-04-10 10:50:03 -0400

    Added chapter 5

---
+++ chapter_5/problems.js
@@ -0,0 +1,13 @@
+// 1: Use reduce and concat methods to flatten multiple arrays into one
+
+let arr1 = [1, 2, 3];
+let arr2 = [4, 5, 6];
+let arr3 = [7, 8, 9];
+let arrList = [arr1, arr2, arr3];
+
+function flattenArrays(arrayList){
+    let result = [];
+    
+}
+
+let arr4 = flattenArrays(arrList);
