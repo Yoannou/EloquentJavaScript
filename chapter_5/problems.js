c3175d6a7df748f8870d0d0c0d4b0993c412cb95
Yoannou <yoannou300@gmail.com>
2021-04-11 11:34:56 -0400

    First problem solved chapter 5

--- chapter_5/problems.js
+++ chapter_5/problems.js
@@ -1,5 +1,8 @@
 // 1: Use reduce and concat methods to flatten multiple arrays into one
 
+/* Note that when using reduce without an Initial_Value argument, the
+accumulator is by default the first element, and current value the second */
+let arr0 = [0, 1];
 let arr1 = [1, 2, 3];
 let arr2 = [4, 5, 6];
 let arr3 = [7, 8, 9];
@@ -7,7 +10,10 @@ let arrList = [arr1, arr2, arr3];
 
 function flattenArrays(arrayList){
     let result = [];
-    
+    result = arrayList.reduce((accumulator, current) => {
+        return accumulator.concat(current);
+    })
+    return result;
 }
 
-let arr4 = flattenArrays(arrList);
+let answer1 = flattenArrays(arrList);
